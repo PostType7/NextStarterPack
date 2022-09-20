a63b53328f3ffc1c111a579fe6d4d631748aba9f
Grzesiek <g.durtan@temida.io>
2022-09-20 22:06:17 +0200

    add forms sanitization

--- src/helpers/P7StateForm.ts
+++ src/helpers/P7StateForm.ts
@@ -9,20 +9,37 @@
 */
 export const fieldKey = (form: any[]) => form[0];
 
-export const formSet = (
-  form: any[],
-  value: string | number
-) => {
+export const formSet = (form: any[], value: string | number) => {
   /* deep mode */
   const out = Object.assign({}, form[1]);
   deepSet(out, form[0], value);
   form[2](out);
   /* flat mode */
-  /* form[2]({ ...form[1], ...{ [form[0]]: e.target.value } }); */ 
+  /* form[2]({ ...form[1], ...{ [form[0]]: e.target.value } }); */
 };
 
 export const formGet = (form: any[]) => {
   return deepGet(form[1], form[0]);
+};
+
+/* 
+  slice data object to parrent object 
+  usage: when responce data is bigger than data to PUT (update form case study)
+  source: https://www.30secondsofcode.org/js/s/walk-through
+*/
+export const sanitData = (pattern: any, data: any) => {
+  const w: any = (x: any, prev = []) => {
+    for (let k of Object.keys(x)) {
+      if (typeof x[k] === "object") w(x[k], [...prev, k]);
+      else
+        deepSet(
+          pattern,
+          [...prev, k].join("."),
+          deepGet(data, [...prev, k].join("."))
+        );
+    }
+  };
+  w(pattern);
 };
 
 export const deepSet = (obj: any, path: string, val: any) => {
@@ -43,6 +60,6 @@ export const deepGet = (obj: any, path: string) => {
       : path,
     v,
     i;
-  for (v = obj, i = 0; v && i < e.length; ++i) v = v[e[i]]
+  for (v = obj, i = 0; v && i < e.length; ++i) v = v[e[i]];
   return v;
 };
